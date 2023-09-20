4c99f6bf99007aa94fd7b531ad7a7adb21594b13
Bajracharya <rejan.bajracharya@cotiviti.com>
2023-09-20 14:27:11 +0545

    fix: translate to eng

--- src/Weather.java
+++ src/Weather.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.util.function.DoubleToIntFunction;
+
 public class Weather {
     public static void main(String[] args) throws InterruptedException, Exception {
         WebDriver driver = new ChromeDriver();
@@ -16,20 +18,23 @@ public class Weather {
             searchField.sendKeys("temperature near me");
             searchField.sendKeys(Keys.ENTER);
 
+            WebElement translate = driver.findElement(By.xpath("//*[text()='Change to English']"));
+            translate.click();
+
             WebElement currentTemp = driver.findElement(By.xpath("//*[@id='wob_tm']"));
+            WebElement tempUnit = driver.findElement(By.xpath("//*[@aria-label='°Celsius']"));
             WebElement localTime = driver.findElement(By.xpath("//*[@id='wob_dts']"));
             WebElement location = driver.findElement(By.xpath("//*[@class='BBwThe']"));
 
 
+            System.out.println(tempUnit);
             System.out.println("Location:" + location.getText());
             System.out.println("Current Time: " + localTime.getText());
-            System.out.println("Temperature atm: " + currentTemp.getText());
+            System.out.println("Temperature atm: " + currentTemp.getText() + tempUnit.getText());
 
-            Thread.sleep(500);
-            driver.close();
-        }catch(InterruptedException e){
-            System.out.println("Timeout");
-        }catch(Exception e) {
+//            Thread.sleep(500);
+//            driver.close();
+        } catch(Exception e) {
             System.out.println("Something went wrong!");
             System.out.println(e.getStackTrace());
         }
